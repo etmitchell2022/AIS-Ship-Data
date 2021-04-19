d4f0095af0839831df7ae8134cc707a70dec1100
Evan Mitchell <etmitchell@bsu.edu>
2021-04-19 14:36:20 -0400

    Add Nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Map from './components/Map';
+import Nav from './components/Nav';
 
 function App() {
   const [shipData, setShipData] = useState(null);
 
   return (
     <div className='App'>
+      <Nav />
       <Map />
     </div>
   );
